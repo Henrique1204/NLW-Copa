4880d4f4f45d9c0799009d80a4df475f64ed66e5
Paulo Souza <paulosouza@HL-SP1016.local>
2022-11-05 15:58:53 -0300

    Criar setup para o backend

---
+++ server/src/server.ts
@@ -0,0 +1,32 @@
+import Fastify from "fastify";
+import cors from '@fastify/cors';
+
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient({
+    log: ['query'],
+});
+
+const bootstrap = async () => {
+    const fastify = Fastify({
+        logger: true,
+    });
+
+    await fastify.register(cors, {
+        origin: true
+    });
+
+    fastify.get('/pools/count', async () => {
+        const count = await prisma.pool.count();
+
+        return { count };
+    });
+
+    await fastify.listen({
+        port: 3333,
+        // Opção para funcionar no mobile (??)
+        host: '0.0.0.0'
+    });
+};
+
+bootstrap();
