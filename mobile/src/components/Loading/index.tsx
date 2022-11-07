c7f6baa4d099196df8ba7e065242263ba18efbe7
Paulo <pauloh16.pdr@gmail.com>
2022-11-07 04:00:17 -0300

    Adicionar pasta mobile outra vez

---
+++ mobile/src/components/Loading/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Center, Spinner } from 'native-base';
+
+const Loading: React.FC = () => {
+    return (
+        <Center flex={1} bg="gray.900">
+            <Spinner color="yellow.500" />
+        </Center>
+    );
+};
+
+export default Loading;
