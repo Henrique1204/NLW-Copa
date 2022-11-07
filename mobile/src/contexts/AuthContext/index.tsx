3c15d6e55830f45806a340e72bca833edbd031a6
Paulo <pauloh16.pdr@gmail.com>
2022-11-07 05:38:34 -0300

    implementar autenticacao com google

--- mobile/src/contexts/AuthContext/index.tsx
+++ mobile/src/contexts/AuthContext/index.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 
+import * as Google from 'expo-auth-session/providers/google';
+import * as AuthSession from 'expo-auth-session';
+import * as WebBrowser from 'expo-web-browser';
+
+import { GOOGLE_CLIENT_ID } from '@env';
+
+WebBrowser.maybeCompleteAuthSession();
+
 export interface IUserProps {
     name: string;
     avatarUrl: string;
@@ -7,21 +15,56 @@ export interface IUserProps {
 
 export interface IAuthContextDataProps {
     user: IUserProps;
+    isUserLoading: boolean;
     signIn: () => Promise<void>;
 }
 
 export const AuthContext = React.createContext<IAuthContextDataProps>({} as IAuthContextDataProps);
 
 export const AuthContextProvider: React.IComponent = ({ children }) => {
+    const [isUserLoading, setIsUserLoading] = React.useState<boolean>(false);
+
     const [user, setUser] = React.useState<IUserProps>({
         name: 'Henrique',
         avatarUrl: 'https://github.com/henrique1204.png',
     });
 
-    const signIn = async () => {};
+    const [_, response, promptAsync] = Google.useAuthRequest({
+        clientId: GOOGLE_CLIENT_ID,
+        redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+        scopes: ['profile', 'email'],
+    });
+
+    const signIn = async () => {
+        try {
+            setIsUserLoading(true)
+
+            await promptAsync();
+
+
+        }
+        catch (error) {
+            console.error(error);
+
+            throw error;
+        }
+        finally {
+            setIsUserLoading(false);
+        }
+    };
+
+    const signInWithGoogle = async (access_token: string) => {
+        console.log('access_token', access_token);
+    };
+
+    React.useEffect(() => {
+        if (response?.type === "success" && response?.authentication?.accessToken) {
+            signInWithGoogle(response.authentication.accessToken);
+        }
+    }, [response])
 
     return (
-        <AuthContext.Provider value={{ signIn, user }}>
+        <AuthContext.Provider value={{ signIn, user, isUserLoading }}>
             {children}
         </AuthContext.Provider>
     );
