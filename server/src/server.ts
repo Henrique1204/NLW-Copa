1461cd307da45c278e741b4fc654aa0be9ea3a0f
Paulo <pauloh16.pdr@gmail.com>
2022-11-07 01:42:00 -0300

    Criar rotas para contagem e adicao de boloes

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,9 @@ import cors from '@fastify/cors';
 
 import { PrismaClient } from '@prisma/client';
 
+import { z } from 'zod';
+import ShortUniqueId from 'short-unique-id';
+
 const prisma = new PrismaClient({
     log: ['query'],
 });
@@ -20,6 +23,37 @@ const bootstrap = async () => {
         const count = await prisma.pool.count();
 
         return { count };
+    });
+
+    fastify.get('/users/count', async () => {
+        const count = await prisma.user.count();
+
+        return { count };
+    });
+
+    fastify.get('/guesses/count', async () => {
+        const count = await prisma.guess.count();
+
+        return { count };
+    });
+
+    fastify.post('/pools', async ({ body }, reply) => {
+        const createPoolBody = z.object({
+            title: z.string(),
+        });
+
+        const { title } = createPoolBody.parse(body);
+
+        const generateCode = new ShortUniqueId({ length: 6 });
+
+        const pool = await prisma.pool.create({
+            data: {
+                title,
+                code: String(generateCode()).toUpperCase(),
+            }
+        })
+
+        return reply.status(201).send(pool);
     });
 
     await fastify.listen({
