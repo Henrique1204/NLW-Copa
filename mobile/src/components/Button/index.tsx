b99f1231d19849ac5d19b497d739fdd8a9d124c0
Paulo <pauloh16.pdr@gmail.com>
2022-11-07 04:37:54 -0300

    Criar tela de sign in

---
+++ mobile/src/components/Button/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import { Button as ButtonNB, IButtonProps as IButtonNBProps, Text } from 'native-base';
+
+interface IButtonProps extends IButtonNBProps {
+    title: string;
+    type?: 'PRIMARY' | 'SECONDARY';
+}
+
+const Button: React.FC<IButtonProps> = ({ title, type = "PRIMARY", ...props }) => {
+    return (
+        <ButtonNB
+            w="full"
+            h={14}
+            rounded="sm"
+            fontSize="md"
+            bg={type === "SECONDARY" ? 'red.500' : 'yellow.500'}
+            _pressed={{
+                bg: type === "SECONDARY" ? 'red.600' : 'yellow.600',
+            }}
+            {...props}
+        >
+            <Text
+                textTransform="uppercase"
+                rounded="sm"
+                fontFamily="heading"
+                fontSize="md"
+                color={type === "SECONDARY" ? 'white' : 'black'}
+            >
+                {title}
+            </Text>
+        </ButtonNB>
+    );
+};
+
+export default Button;
--- mobile/src/screens/SignIn/index.tsx
+++ mobile/src/screens/SignIn/index.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import { Fontisto } from '@expo/vector-icons';
 
-import { Center, Text } from 'native-base';
+import { Center, Icon, Text } from 'native-base';
+
+import Logo  from '../../assets/logo.svg';
+import Button from '../../components/Button';
 
 const SignIn: React.FC = () => {
   return (
-      <Center flex={1} bgColor="gray.900">
-        <Text color="white" fontSize={24}>Hello World Native</Text>
+      <Center flex={1} bgColor="gray.900" p={7}>
+        <Logo width={212} height={40} />
+
+        <Button
+          type="SECONDARY"
+          title='Entrar com google'
+          mt="12"
+          leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+        />
+
+        <Text color="white" textAlign="center" mt="4">
+          Não utilizamos nenhuma informação além {'\n'} do seu e-mail para criação de sua conta.
+        </Text>
       </Center>
   );
 }
